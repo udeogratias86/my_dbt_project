00c42d227e4873fab468b14e88da6175aca1d3ed
udeogratias <u.deogratias@gmail.com>
2025-10-31 20:30:32 +0000

    fix_date bugs

--- models/mart/local_bike/mrt_dim_customers.sql
+++ models/mart/local_bike/mrt_dim_customers.sql
@@ -1,10 +1,9 @@
 select
   customer_id, 
-  first_name, 
-  last_name, 
-  email, 
+  first_name as customer_first_name, 
+  last_name as customer_last_name, 
+  trim(concat(coalesce(first_name, ''), ' ', coalesce(last_name, ''))) as customer_full_name,
   city, 
   state, 
   zip_code
 from {{ ref('int_local_bike__customer') }}
-
