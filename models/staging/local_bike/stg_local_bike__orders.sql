91fe10a925c7f62fc22319bb98d050196543ee34
udeogratias <u.deogratias@gmail.com>
2025-10-31 20:46:46 +0000

    fix_shipped_date

--- models/staging/local_bike/stg_local_bike__orders.sql
+++ models/staging/local_bike/stg_local_bike__orders.sql
@@ -4,7 +4,7 @@ select
   order_status,
   cast(order_date as date) as order_date,
   cast(required_date as date) as required_date,
-  safe.parse_date('%y-%m-%d', nullif(shipped_date, 'NULL')) as shipped_date,
+  safe.parse_date('%Y-%m-%d', nullif(shipped_date, 'NULL')) as shipped_date,
   store_id,
   staff_id
  from {{ source('local_bike', 'orders') }}
